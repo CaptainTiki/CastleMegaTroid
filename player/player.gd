9223751c381cfdda0ec189b33eb8ac89d5476b33
CptTiki <CptTiki@gmail.com>
2026-07-12 17:56:52 -0400

    PlayerState

--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,12 @@
 extends CharacterBody3D
 class_name Player
 
+#region /// State Machine Variables ///
+var states : Array[ PlayerState ]
+#endregion
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 8
-const GRAVITY = -16
+func _process(delta: float) -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
-	velocity.x = 0
-	if Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-	elif Input.is_action_pressed("right"):
-		velocity.x = SPEED
-	velocity.y = velocity.y + GRAVITY * delta
-
-	move_and_slide()
+	pass
---
+++ player/states/00_player_state.gd
@@ -0,0 +1,29 @@
+@icon("uid://dsmiqgkibw5mn")
+extends Node
+class_name PlayerState
+
+
+var player : Player
+var next_state : PlayerState
+
+#region /// state references ///
+# reference to all other states
+#endregion
+
+func init() -> void:
+	pass
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func handle_input( _event : InputEvent ) -> PlayerState:
+	return next_state
+
+func process(_delta: float) -> PlayerState:
+	return next_state
+
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
